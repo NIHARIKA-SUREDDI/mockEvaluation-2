7e989dc0c2a3a44ea7716049733287cb64eafd45
NIHARIKA-SUREDDI <niharika.sureddi@gmail.com>
2024-12-26 16:59:29 +0530

    form updated

---
+++ baseUrl.js
@@ -0,0 +1,1 @@
+export const baseUrl="https://silk-four-friend.glitch.me/questions"
---
+++ index1.js
@@ -0,0 +1,10 @@
+import { baseUrl } from "./baseUrl.js";
+
+let form =document.getElementById("form");
+form.addEventListener("submit",function(){
+    event.preventDefault();
+    let email=email.form.value;
+    let password=form.password.value;
+})
+
+
